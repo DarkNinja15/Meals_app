c8de4b9e5365ed0b651dd41eb98192e19e9566ef
Animesh Shukla <animeshshukla1518@gmail.com>
2022-05-09 22:31:07 +0530

    GridView Added

--- meals/lib/main.dart
+++ meals/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:meals/screens/categories_screen.dart';
 
 import 'home_page.dart';
 
@@ -13,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'MealTime',
-      home: HomePage(),
+      home: CategoriesScreen(),
     );
   }
 }
--- meals/lib/screens/categories_screen.dart
+++ meals/lib/screens/categories_screen.dart
@@ -1,17 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:meals/category_item.dart';
+
+import '../models/dummy_data.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-          maxCrossAxisExtent: 200,
-          childAspectRatio: 1.5,
-          crossAxisSpacing: 20,
-          mainAxisSpacing: 20),
-      children: [],
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('MealTime'),
+      ),
+      body: GridView(
+          gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+              maxCrossAxisExtent: 200,
+              childAspectRatio: 1.5,
+              crossAxisSpacing: 20,
+              mainAxisSpacing: 20),
+          children: [
+            ...DUMMY_CATEGORIES
+                .map((e) => CategoryItem(title: e.title, color: e.color))
+                .toList()
+          ]),
     );
   }
 }
